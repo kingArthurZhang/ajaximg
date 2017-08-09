12abf55a44f7ea24a472fd153f6eae62d7c76764
Arthur <Arthur199706@outlook.com>
2017-08-09 18:36:32 +0800

    AJAX effect

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
-echo "<img src='a.jpg'>";
-?>
+$username=$_POST['username'];
+$password=$_POST['password'];
+
+if($username=="admin" && $password=="123"){
+    echo "yes";
+}else{
+    echo "no";
+}
